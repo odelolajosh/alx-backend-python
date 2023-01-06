79c4376ca6929c88069d4715ad4089e74c0fbcf3
odelolajosh <oladapoodelola@gmail.com>
2023-01-06 15:31:01 +0100

    fix

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ returns the sum of a list of float and integers. """
     return sum(mxd_lst)
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ element_length """
-from collections.abc import Iterable, List, Tuple, Sequence
+from typing import Iterable, List, Tuple, Sequence
 
 
-def elment_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ returns a list of tuple with element and length of element. """
     return [(i, len(i)) for i in lst]
