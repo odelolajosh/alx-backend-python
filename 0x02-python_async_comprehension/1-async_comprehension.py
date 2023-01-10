e6afc3afe1400e0c7782beeedfe86d614a00eaad
odelolajosh <oladapoodelola@gmail.com>
2023-01-10 23:08:47 +0100

    task: async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" 1. Async Comprehensions """
+import asyncio
+from typing import List
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    return [n async for n in async_generator()]
