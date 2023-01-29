f9e7aff2d6f5a6e1af3ea759ca360aed16010ec3
odelolajosh <oladapoodelola@gmail.com>
2023-01-29 09:32:37 +0100

    taskL test `public_repos()` method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -10,10 +10,10 @@ class TestGithubOrgClient(unittest.TestCase):
     """ Test GithubOrgClient """
 
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google"),
+        ("abc")
     ])
-    @patch('client.get_json')
+    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
         """ Test org """
         expected = {"name": org_name}
@@ -26,11 +26,24 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """ Test public repos url """
-        with patch('client.GithubOrgClient.org',
+        with patch("client.GithubOrgClient.org",
                    new_callable=PropertyMock) as org_property_mock:
             expected = {
-                'repos_url': 'https://api.github.com/orgs/google/repos'}
+                "repos_url": "https://api.github.com/orgs/google/repos"}
             org_property_mock.return_value = expected
-            client = GithubOrgClient('google')
-            self.assertEqual(client._public_repos_url, expected['repos_url'])
+            client = GithubOrgClient("google")
+            self.assertEqual(client._public_repos_url, expected["repos_url"])
             org_property_mock.assert_called_once()
+
+    @patch("client.get_json")
+    def test_public_repos(self, get_json_mock: Mock):
+        expected = ["alx-backend", "alx-backend-python"]
+        get_json_mock.return_value = [{"name": repo} for repo in expected]
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as public_repos_url_mock:
+            test_url = "https://api.github.com/orgs/google/repos"
+            public_repos_url_mock.return_value = test_url
+            client = GithubOrgClient("google")
+            self.assertEqual(client.public_repos(),
+                             ["alx-backend", "alx-backend-python"])
+            get_json_mock.assert_called_once_with(test_url)
